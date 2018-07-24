a164e20b378dbf8a9064894aba2af6a82af058c8
aleemzaki <aleemzaki@gmail.com>
2018-07-23 18:56:14 -0700

    saving prog

--- processEvents.py
+++ processEvents.py
@@ -15,6 +15,7 @@ def processInput(input_file):
 	fin = open(input_file, "r")
 	fin2 = open(input_file, "r")
 	
+	
 
 	constraintDict = {}
 
@@ -24,32 +25,30 @@ def processInput(input_file):
 	# the simulation to one pair of start and stop locations.
 	reducedTrips = []
 
-		#currentConstraint = []
-
 	for i in range(220):
-			constraint = fin.readline().split(",")
+		constraint = fin.readline().split(",")
 
-			c = constraint
-			m = [k for v,k in enumerate(constraint)]
+		c = constraint
+		m = [k for v,k in enumerate(constraint)]
+		constraintDict[i] = c
+		timestamp = int(str(m[0]))
+		bird_id = str(m[1])
+		event_type = str(m[2])
+		x = float(str(m[3]))
+		y = float(str(m[4]))
+		user_id = str(m[5])
 
-			constraintDict[i] = c
-			timestamp = int(str(m[0]))
-			bird_id = str(m[1])
-			event_type = str(m[2])
-			x = float(str(m[3]))
-			y = float(str(m[4]))
-			user_id = str(m[5])
+		if i == 222:
+			print(timestamp+", "+bird_id+", "+event_type+", "+x+", "+y+", "+user_id)
 
-			if i == 222:
-				print(timestamp+", "+bird_id+", "+event_type+", "+x+", "+y+", "+user_id)
-
-			if user_id not in users:
-				users.append(user_id)
-			if event_type == 'DROP':
-				reducedTrips.append((bird_id,x,y,x,y,timestamp,0))
-				birds.append(bird_id)
-
-	for i in range(220):
+		if user_id not in users:
+			users.append(user_id)
+		if event_type == 'DROP':
+			reducedTrips.append((bird_id,x,y,x,y,timestamp,0))
+			birds.append(bird_id)
+	for h in range(220):
+		fin2 = open(input_file, "r")
+		for i in range(220):
 			constraint = fin2.readline().split(",")
 
 			c = constraint
@@ -62,20 +61,20 @@ def processInput(input_file):
 			x = float(str(m[3]))
 			y = float(str(m[4]))
 			user_id = str(m[5])			
-			if event_type == 'END_RIDE':
-				#print('bingo!')
-				j=0
-				tempCopy = reducedTrips
-				for tupleV in reducedTrips:
-					if tupleV[0] == bird_id:
-						#print('bingo!')
-						if int(tupleV[5]) < int(timestamp):
-							lst = list(tupleV)
-							lst[3] = x
-							lst[4] = y
-							t = tuple(lst)
-							reducedTrips[j]=t
-							#reducedTrips = tempCopy
+			#if event_type == 'END_RIDE':
+			#print('bingo!')
+			j=0
+			tempCopy = reducedTrips
+			for tupleV in reducedTrips:
+				if tupleV[0] == bird_id:
+					#print('bingo!')
+					if int(tupleV[5]) < int(timestamp):
+						lst = list(tupleV)
+						lst[3] = x
+						lst[4] = y
+						t = tuple(lst)
+						reducedTrips[j]=t
+						#reducedTrips = tempCopy
 	jk=0
 	for tupleVI in reducedTrips:
 		tempCopy2 = reducedTrips
