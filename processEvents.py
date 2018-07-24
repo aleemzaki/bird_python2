f21878ccd39eb1b95f1f534ed30b036e986732f5
aleemzaki <aleemzaki@gmail.com>
2018-07-23 18:20:53 -0700

    saving prog

--- processEvents.py
+++ processEvents.py
@@ -8,8 +8,7 @@ def main(argv):
 	#	print("Usage: python phase2algorithm.py [path_to_input#.py] [path_to_output#.py]")
 	#	return#need to create .out beforehand
 	#print "Hey!"
-	#finalOrder = 
-	processInput(argv[0])
+	finalOrder = processInput(argv[0])
 	#print finalOrder
 
 def processInput(input_file):
@@ -34,11 +33,11 @@ def processInput(input_file):
 			m = [k for v,k in enumerate(constraint)]
 
 			constraintDict[i] = c
-			timestamp = str(m[0])
+			timestamp = int(str(m[0]))
 			bird_id = str(m[1])
 			event_type = str(m[2])
-			x = str(m[3])
-			y = str(m[4])
+			x = float(str(m[3]))
+			y = float(str(m[4]))
 			user_id = str(m[5])
 
 			if i == 222:
@@ -57,7 +56,7 @@ def processInput(input_file):
 			m = [k for v,k in enumerate(constraint)]
 
 			constraintDict[i] = c
-			timestamp = str(m[0])
+			timestamp = int(str(m[0]))
 			bird_id = str(m[1])
 			event_type = str(m[2])
 			x = float(str(m[3]))
@@ -66,9 +65,9 @@ def processInput(input_file):
 			if event_type == 'END_RIDE':
 				#print('bingo!')
 				j=0
-				#tempCopy = reducedTrips
+				tempCopy = reducedTrips
 				for tupleV in reducedTrips:
-					if tupleV[0] == 'JUVS':
+					if tupleV[0] == bird_id:
 						#print('bingo!')
 						if int(tupleV[5]) < int(timestamp):
 							lst = list(tupleV)
@@ -79,10 +78,10 @@ def processInput(input_file):
 							#reducedTrips = tempCopy
 	jk=0
 	for tupleVI in reducedTrips:
-		#tempCopy2 = reducedTrips
+		tempCopy2 = reducedTrips
 		lst = list(tupleVI)
 		print(tupleVI[1])
-		lst[6] = math.sqrt(((float(tupleVI[1])-float(tupleVI[3]))**2)-((float(tupleVI[2])-float(tupleVI[4]))**2))
+		lst[6] = math.sqrt((float(tupleVI[1])-float(tupleVI[3]))**2 -(float(tupleVI[2])-float(tupleVI[4]))**2)
 		#(((float(tupleVI[1])-(float(tupleVI[3]))**2)*math.sin(math.radians(float(tupleVI[3])))+math.cos(math.radians(float(tupleVI[1]))))#*math.cos(math.radians(float(tupleVI[3])))*math.cos(math.radians(float(tupleVI[4]))-math.radians(float(tupleVI[2]))))*6371
 		t = tuple(lst)
 		reducedTrips[jk]=t
